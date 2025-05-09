57b9516c263637521d52089a1d17bca1cfc9feb1
Selim735 <benslimensalim32@gmail.com>
2025-05-10 00:27:58 +0100

    feat: Add ServicesSection component with service offerings and icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+
 import Hero from "@/components/Hero";
+import ServiceSection from "@/components/Service";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <Hero/>
+      <ServiceSection/>
     </div>
   );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -134,7 +134,7 @@ export default function Hero() {
       </div>
       
       {/* Animation wave en bas */}
-      <div className="absolute bottom-0 left-0 w-full">
+      {/* <div className="absolute bottom-0 left-0 w-full">
         <svg viewBox="0 0 1200 120" preserveAspectRatio="none" className="h-16 w-full">
           <path 
             d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" 
@@ -151,7 +151,7 @@ export default function Hero() {
             fill="#ffffff"
           ></path>
         </svg>
-      </div>
+      </div> */}
       
       {/* Global styles */}
       <style jsx global>{`
